847aedd9ad52fc1a47e7fda2cda871dfadc0147c
Ashutosh Jha <ashutosh.j@tradelab.in>
2020-11-07 14:12:32 +0530

    call issue fixed

--- app/src/main/java/lets/digi/talk/util/AsyncHttpURLConnection.java
+++ app/src/main/java/lets/digi/talk/util/AsyncHttpURLConnection.java
@@ -6,7 +6,16 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * Asynchronous http requests implementation.
@@ -48,8 +57,39 @@ public class AsyncHttpURLConnection {
     new Thread(runHttp).start();
   }
 
+  private static void disableSSLCertificateChecking() {
+    TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+      public X509Certificate[] getAcceptedIssuers() {
+        return null;
+      }
+
+      @Override
+      public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+        // Not implemented
+      }
+
+      @Override
+      public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+        // Not implemented
+      }
+    } };
+
+    try {
+      SSLContext sc = SSLContext.getInstance("TLS");
+
+      sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+      HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    } catch (KeyManagementException e) {
+      e.printStackTrace();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+  }
+
   private void sendHttpMessage() {
     try {
+      disableSSLCertificateChecking();
       HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
       byte[] postData = new byte[0];
       if (message != null) {
